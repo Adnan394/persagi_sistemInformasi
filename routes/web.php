897d2f444afa938a01e3b2407caf7b8632a8026f
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-08-03 20:22:36 +0700

    tambah routes anggota

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.dashboard');
 });
+
+Route::get('anggota', function () {
+    return view('anggota.dashboard');
+});
