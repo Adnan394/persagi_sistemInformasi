e432efe3886c612a2f7b80751b1cca535f8bd0f0
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-08-06 14:45:26 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,7 +53,7 @@
                     <div class="d-flex justify-content-center py-4">
                       <a href="index.html" class="logo d-flex align-items-center w-auto">
                         <img src="assets/img/logo.png" alt="">
-                        <span class="d-none d-lg-block">NiceAdmin</span>
+                        <span class="d-none d-lg-block">Persagi Karawang</span>
                       </a>
                     </div><!-- End Logo -->
       
@@ -62,8 +62,8 @@
                       <div class="card-body">
       
                         <div class="pt-4 pb-2">
-                          <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                          <p class="text-center small">Enter your username & password to login</p>
+                          <h5 class="card-title text-center pb-0 fs-4">Login  untuk Masuk Kedalam Akun</h5>
+                          <p class="text-center small">Masukan Email dan Password</p>
                         </div>
       
                         <form action="{{ route('login.store') }}" method="POST" class="row g-3 needs-validation" novalidate>
@@ -83,17 +83,17 @@
                             <div class="invalid-feedback">Please enter your password!</div>
                           </div>
       
-                          <div class="col-12">
+                          {{-- <div class="col-12">
                             <div class="form-check">
                               <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
                               <label class="form-check-label" for="rememberMe">Remember me</label>
                             </div>
-                          </div>
-                          <div class="col-12">
+                          </div> --}}
+                          <div class="col-12 pt-2">
                             <button class="btn btn-primary w-100" type="submit">Login</button>
                           </div>
                           <div class="col-12">
-                            <p class="small mb-0">Don't have account? <a href="{{ route('register.index') }}">Create an account</a></p>
+                            <p class="small mb-0">Belum punya akun Anggota ? <a href="{{ route('register.index') }}">Buat Akun</a></p>
                           </div>
                         </form>
       
