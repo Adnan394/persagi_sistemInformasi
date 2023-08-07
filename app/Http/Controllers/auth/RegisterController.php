5e85675b8e0d4781ffca27d874849451d92540ca
adnan <adnanega82@gmail.com>
2023-08-07 16:24:38 +0700

    [feature] activate account

--- app/Http/Controllers/auth/RegisterController.php
+++ app/Http/Controllers/auth/RegisterController.php
@@ -36,6 +36,6 @@ class RegisterController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        return redirect()->route('login.index');
+        return redirect()->route('login');
     }
 }
